812d8ac39ee0b531635ff84f2e339257abfbb23f
adarsh-vishwakarma <adarshvishwakarma704@gmail.com>
2024-10-19 09:53:22 +0530

    mobile navbar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,23 @@
 import { PiggyBank } from "lucide-react";
 
-const Logo = () => {
+export const Logo = () => {
   return (
     <a href="/" className="flex items-center gap-2">
       <PiggyBank className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" />
-      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">BudgetTracker</p>
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        BudgetTracker
+      </p>
     </a>
   );
 };
 
-export default Logo;
+export const LogoMobile = () => {
+  return (
+    <a href="/" className="flex items-center gap-2">
+      
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        BudgetTracker
+      </p>
+    </a>
+  );
+};
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,20 @@
 "use client";
 import { usePathname } from "next/navigation";
-import Logo from "./Logo";
+import { Logo, LogoMobile } from "./Logo";
 import Link from "next/link";
-import { buttonVariants } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+import { ThemeSwitcherBtn } from "./ThemeSwitcherBtn";
+import { useState } from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
 
 const Navbar = () => {
   return (
     <>
       <DesktopNavbar />
+      <MobileNavbar />
     </>
   );
 };
@@ -18,6 +24,43 @@ const items = [
   { label: "Transaction", link: "/transaction" },
   { label: "Manage", link: "/manage" },
 ];
+
+const MobileNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="block border-separate bg-background md:hidden">
+      <nav className="container flex items-center justify-between px-8">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+            <Button variant={"ghost"} size={"icon"}>
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[400px] sm:w-[540px]" side="left">
+            <Logo />
+            <div className="flex flex-col gap-1 pt-4">
+              {items.map((item) => (
+                <NavbarItem
+                  key={item.label}
+                  link={item.link}
+                  label={item.label}
+                  clickCallback={() => setIsOpen((prev) => !prev)}
+                />
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+          <LogoMobile />
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 const DesktopNavbar = () => {
   return (
@@ -35,14 +78,27 @@ const DesktopNavbar = () => {
             ))}
           </div>
         </div>
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
       </nav>
     </div>
   );
 };
 
-const NavbarItem = ({ link, label }: { link: string; label: string }) => {
+const NavbarItem = ({
+  link,
+  label,
+  clickCallback,
+}: {
+  link: string;
+  label: string;
+  clickCallback?: () => void;
+}) => {
   const pathname = usePathname();
   const isActive = pathname === link;
+
   return (
     <div className="relative flex items-center">
       <Link
@@ -52,9 +108,15 @@ const NavbarItem = ({ link, label }: { link: string; label: string }) => {
           "w-full justify-start text-lg text-muted-foreground",
           isActive && "text-foreground"
         )}
+        onClick={() => {
+          if (clickCallback) clickCallback();
+        }}
       >
         {label}
       </Link>
+      {isActive && (
+        <div className="absolute -bottom-[2px] left-1/2 hidden h-[2px] w-[80%] -translate-x-1/2 rounded-xl bg-foreground md:block"></div>
+      )}
     </div>
   );
 };
